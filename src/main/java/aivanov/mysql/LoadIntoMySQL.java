cc8497646fc210567b49bcb2d63016b16f7f96b2
Anton Ivanov <aivanov@twitter.com>
2022-10-30 12:52:28 +0000

    Load into MySQL with LOAD DATA

--- src/main/java/aivanov/mysql/LoadIntoMySQL.java
+++ src/main/java/aivanov/mysql/LoadIntoMySQL.java
@@ -1,7 +1,11 @@
 package aivanov.mysql;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.concurrent.TimeUnit;
 
@@ -12,23 +16,24 @@ import aivanov.SQL;
 class LoadIntoMySQL {
 
   // TODO:
-  private static final int edgesPerBatch = 1_000;
+  private static final int edgesPerBatch = 100_000;
+  public static final Path tempCSV = Path.of("temp.csv");
 
   public static void main(String[] args) throws SQLException, IOException {
     var startNanos = System.nanoTime();
-    var inserted = 0L;
-    var lastInserted = 0L;
+    var parsed = 0L;
+    var lastParsed = 0L;
     var lastPrint = System.nanoTime();
 
     MySQL.recreateDatabase();
     try (var conn = MySQL.createConnection()) {
       SQL.createTable(conn);
 
-      try (var preparedInsert = conn.prepareStatement(SQL.insertQuery)) {
-        var prevSId = 0L;
-        var prevDId = 0L;
-        try (var reader = Files.newBufferedReader(Edges.csvFile)) {
-          conn.setAutoCommit(false);
+      var prevSId = 0L;
+      var prevDId = 0L;
+      try (var reader = Files.newBufferedReader(Edges.csvFile)) {
+        var csvWriter = Files.newBufferedWriter(tempCSV, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        try {
           while (true) {
             var line = reader.readLine();
             if (line == null) {
@@ -40,30 +45,62 @@ class LoadIntoMySQL {
             prevSId = edge.sId;
             prevDId = edge.dId;
 
-            SQL.insert(edge, preparedInsert);
-            inserted++;
-            if (inserted % edgesPerBatch == 0) {
-              conn.commit();
-              conn.setAutoCommit(false);
+            writeToCSV(edge, csvWriter);
+            parsed++;
+
+            if (parsed % edgesPerBatch == 0) {
+              csvWriter.close();
+              fromCSVToDB(conn);
+              csvWriter = Files.newBufferedWriter(tempCSV, StandardOpenOption.TRUNCATE_EXISTING);
             }
 
             var nanoTime = System.nanoTime();
             if (TimeUnit.NANOSECONDS.toSeconds(nanoTime - lastPrint) >= 10) {
-              Edges.printProgress(lastInserted, inserted, Edges.numEdgesInCSVFile, startNanos);
-              lastInserted = inserted;
+              Edges.printProgress(lastParsed, parsed, Edges.numEdgesInCSVFile, startNanos);
+              lastParsed = parsed;
               lastPrint = nanoTime;
             }
           }
-
-          conn.commit();
-
-          // TODO: create indices
-
-          var sec = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - startNanos);
-          var totalPerSec = inserted / sec;
-          System.out.println("Inserted " + inserted + " edges in " + sec + " sec, average speed " + totalPerSec + " / sec");
+          csvWriter.close();
+          fromCSVToDB(conn);
+        } finally {
+          Files.delete(tempCSV);
         }
       }
+
+      // TODO: create indices
+
+      var sec = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - startNanos);
+      var totalPerSec = parsed / sec;
+      System.out.println("Inserted " + parsed + " edges in " + sec + " sec, average speed " + totalPerSec + " / sec");
+    }
+  }
+
+  private static void writeToCSV(Edge edge, BufferedWriter csvWriter) throws IOException {
+    csvWriter.write(
+        String.join(
+            ",",
+            Long.toString(edge.sId),
+            "0",
+            Long.toString(edge.ut),
+            Long.toString(edge.dId),
+            "0",
+            Long.toString(edge.ut),
+            Byte.toString(edge.st),
+            Long.toString(edge.ut),
+            Long.toString(edge.pos),
+            Byte.toString(edge.ann),
+            Byte.toString(edge.st),
+            Long.toString(edge.ut)
+        )
+    );
+    csvWriter.newLine();
+  }
+
+  private static void fromCSVToDB(Connection conn) throws SQLException {
+    // TODO: prepare
+    try(var statement = conn.createStatement()) {
+      statement.execute("LOAD DATA INFILE '" + tempCSV.toAbsolutePath() + "' INTO TABLE edges FIELDS TERMINATED BY ',' (s_id, s_st, s_ut, d_id, d_st, d_ut, st, ut, pos, ann, a_st, a_ut)");
     }
   }
 
